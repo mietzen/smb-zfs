916efb11b0e410caef60992802815381c016bcc9
Nils Stein <31704359+mietzen@users.noreply.github.com>
2025-07-22 16:36:24 +0200

    Check all files

--- smb_zfs/__init__.py
+++ smb_zfs/__init__.py
@@ -1,5 +1,6 @@
+# ruff: noqa: F401
 from .errors import SmbZfsError
-from .const import *
+from .const import STATE_FILE, SMB_CONF, AVAHI_SMB_SERVICE
 from .zfs import ZFS
 from .system import System
 from .state_manager import StateManager
